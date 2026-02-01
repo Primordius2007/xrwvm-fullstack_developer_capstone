0c6a2015212daa879ff81c67b60a5bdb7f8de7fd
Primordius2007 <139982666+Primordius2007@users.noreply.github.com>
2026-02-01 20:56:55 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,21 +1,40 @@
-# Uncomment the imports before you add the code
-from django.urls import path
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
 from . import views
 
-app_name = 'djangoapp'
+app_name = "djangoapp"
 
 urlpatterns = [
-    # path for login
-    path(route='login', view=views.login_user, name='login'),
+    # ---------- AUTH ----------
+    path("login/", views.login_user, name="login"),
 
-    # path for get cars (CarMake & CarModel)
-    path(route='get_cars', view=views.get_cars, name='getcars'),
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    path(route='add_review', view=views.add_review, name='add_review'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    # ---------- CARS ----------
+    path("get_cars/", views.get_cars, name="getcars"),
 
+    # ---------- DEALERS ----------
+    path(
+        "get_dealers/",
+        views.get_dealerships,
+        name="get_dealers",
+    ),
+    path(
+        "get_dealers/<str:state>/",
+        views.get_dealerships,
+        name="get_dealers_by_state",
+    ),
+    path(
+        "dealer/<int:dealer_id>/",
+        views.get_dealer_details,
+        name="dealer_details",
+    ),
+    path(
+        "reviews/dealer/<int:dealer_id>/",
+        views.get_dealer_reviews,
+        name="dealer_reviews",
+    ),
+
+    # ---------- REVIEWS ----------
+    path("add_review/", views.add_review, name="add_review"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
