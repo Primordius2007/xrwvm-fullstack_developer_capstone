caf8773a2ea86944ba3a957989fdb2cdb58235b0
Primordius2007 <139982666+Primordius2007@users.noreply.github.com>
2026-02-01 18:32:45 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,3 +1,4 @@
+# Uncomment the imports before you add the code
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
@@ -6,6 +7,15 @@ from . import views
 app_name = 'djangoapp'
 
 urlpatterns = [
-    path('login', views.login_user, name='login'),
-    path('get_cars', views.get_cars, name='get_cars'),
+    # path for login
+    path(route='login', view=views.login_user, name='login'),
+
+    # path for get cars (CarMake & CarModel)
+    path(route='get_cars', view=views.get_cars, name='getcars'),
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='add_review', view=views.add_review, name='add_review'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
