b541ef715b881da5b4ce231adc19fa654021d3ae
Primordius2007 <139982666+Primordius2007@users.noreply.github.com>
2026-02-01 22:26:57 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -119,8 +119,6 @@ def registration(request):
     """
     return JsonResponse({"status": "Registered"})
 
-
-
 def get_dealerships(request, state="All"):
     """
     Fetch all dealerships or filter by state
