4b4b4e319787c1e582e746ae0411610196777493
Primordius2007 <139982666+Primordius2007@users.noreply.github.com>
2026-02-01 22:18:12 +0530

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -6,20 +6,62 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     # Django admin
-    path('admin/', admin.site.urls),
+    path(
+        "admin/",
+        admin.site.urls,
+    ),
 
     # React frontend routes (SPA)
-    path('', TemplateView.as_view(template_name="index.html")),
-    path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
-    path('login/', TemplateView.as_view(template_name="index.html")),
-    path('register/', TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
+    path(
+        "",
+        TemplateView.as_view(
+            template_name="index.html",
+        ),
+    ),
+    path(
+        "dealers/",
+        TemplateView.as_view(
+            template_name="index.html",
+        ),
+    ),
+    path(
+        "dealer/<int:dealer_id>",
+        TemplateView.as_view(
+            template_name="index.html",
+        ),
+    ),
+    path(
+        "login/",
+        TemplateView.as_view(
+            template_name="index.html",
+        ),
+    ),
+    path(
+        "register/",
+        TemplateView.as_view(
+            template_name="index.html",
+        ),
+    ),
+    path(
+        "postreview/<int:dealer_id>",
+        TemplateView.as_view(
+            template_name="index.html",
+        ),
+    ),
 
     # Django backend APIs
-    path('djangoapp/', include('djangoapp.urls')),
+    path(
+        "djangoapp/",
+        include("djangoapp.urls"),
+    ),
 ]
 
 # Static & media files
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
+urlpatterns += static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT,
+)
