9cc4f55903607e7ff8ed6aec7d1fcd2740762b18
Primordius2007 <139982666+Primordius2007@users.noreply.github.com>
2026-02-01 16:34:55 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,65 +1,39 @@
-# Uncomment the required imports before adding the code
-
-# from django.shortcuts import render
-# from django.http import HttpResponseRedirect, HttpResponse
-# from django.contrib.auth.models import User
-# from django.shortcuts import get_object_or_404, render, redirect
-# from django.contrib.auth import logout
-# from django.contrib import messages
-# from datetime import datetime
-
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
-import logging
-import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
+import json
+import logging
 
+from .models import CarMake, CarModel
+from .populate import initiate
 
-# Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-
-# Create your views here.
-
-# Create a `login_request` view to handle sign in request
 @csrf_exempt
 def login_user(request):
-    # Get username and password from request.POST dictionary
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
-    # Try to check if provide credential can be authenticated
     user = authenticate(username=username, password=password)
-    data = {"userName": username}
+
     if user is not None:
-        # If user is valid, call login method to login current user
         login(request, user)
-        data = {"userName": username, "status": "Authenticated"}
-    return JsonResponse(data)
+        return JsonResponse({"userName": username, "status": "Authenticated"})
 
-# Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+    return JsonResponse({"userName": username, "status": "Failed"})
 
-# Create a `registration` view to handle sign up request
-# @csrf_exempt
-# def registration(request):
-# ...
 
-# # Update the `get_dealerships` view to render the index page with
-# a list of dealerships
-# def get_dealerships(request):
-# ...
+def get_cars(request):
+    if CarMake.objects.count() == 0:
+        initiate()
 
-# Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
-# ...
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
 
-# Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
-# ...
+    for car in car_models:
+        cars.append({
+            "CarMake": car.car_make.name,
+            "CarModel": car.name
+        })
 
-# Create a `add_review` view to submit a review
-# def add_review(request):
-# ...
+    return JsonResponse({"CarModels": cars})
