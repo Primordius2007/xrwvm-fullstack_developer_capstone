344ee16c0248572e3a201cb44a2ad071935482bb
Primordius2007 <139982666+Primordius2007@users.noreply.github.com>
2026-02-01 21:05:16 +0530

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,17 +1,55 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+from . import views
+
+app_name = "djangoapp"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # ---------- AUTH ----------
+    path(
+        "login/",
+        views.login_user,
+        name="login",
+    ),
 
-    # React routes (IMPORTANT)
-    path('login/', TemplateView.as_view(template_name="index.html")),
-    path('register/', TemplateView.as_view(template_name="index.html")),
-    path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
+    # ---------- CARS ----------
+    path(
+        "get_cars/",
+        views.get_cars,
+        name="getcars",
+    ),
 
-    # Django backend APIs
-    path('djangoapp/', include('djangoapp.urls')),
-]
+    # ---------- DEALERS ----------
+    path(
+        "get_dealers/",
+        views.get_dealerships,
+        name="get_dealers",
+    ),
+    path(
+        "get_dealers/<str:state>/",
+        views.get_dealerships,
+        name="get_dealers_by_state",
+    ),
+    path(
+        "dealer/<int:dealer_id>/",
+        views.get_dealer_details,
+        name="dealer_details",
+    ),
+    path(
+        "reviews/dealer/<int:dealer_id>/",
+        views.get_dealer_reviews,
+        name="dealer_reviews",
+    ),
+
+    # ---------- REVIEWS ----------
+    path(
+        "add_review/",
+        views.add_review,
+        name="add_review",
+    ),
+] + static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
